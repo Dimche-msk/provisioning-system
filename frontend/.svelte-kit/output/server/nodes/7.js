33bd2bd8d09bcb8ca578f14b6e549b7c28458233
Dima <d.teyblum@gmail.com>
2026-02-22 00:35:06 +0300

    Updated frontend dependencies and generated SvelteKit files, and modified PBX configuration files.

--- frontend/.svelte-kit/output/server/nodes/7.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 7;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/system/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/7.DqmhF3nK.js","_app/immutable/chunks/BJwbOLc5.js","_app/immutable/chunks/CkE99THf.js","_app/immutable/chunks/C4BPeCQY.js","_app/immutable/chunks/B6JWmkPs.js","_app/immutable/chunks/Cde22M6G.js","_app/immutable/chunks/DLDfI3bs.js","_app/immutable/chunks/DLIjLIs0.js","_app/immutable/chunks/DkvEdztk.js","_app/immutable/chunks/BUuw-r5I.js","_app/immutable/chunks/CiJqPFaz.js","_app/immutable/chunks/mTymgXgU.js","_app/immutable/chunks/Cb12aucO.js"];
-export const stylesheets = ["_app/immutable/assets/Toaster.DQwrSZtH.css"];
-export const fonts = [];
